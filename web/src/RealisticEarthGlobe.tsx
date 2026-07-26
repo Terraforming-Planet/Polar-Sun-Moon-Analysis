c3d0400282b0e63a5d08e1ad0d63722366df1a40
Terraforming Planet <kontakt@teslaeco.pl>
2026-07-26 04:04:21 +0100

    Synchronize global satellite source registry with Earth viewer

--- web/src/RealisticEarthGlobe.tsx
+++ web/src/RealisticEarthGlobe.tsx
@@ -1,1 +1,14 @@
-export { RealisticEarthGlobe } from './CleanRealisticEarthGlobe'
+import { RealisticEarthGlobe as CleanEarthViewer } from './CleanRealisticEarthGlobe'
+import { SatelliteSourceRegistry } from './satellite-source-registry'
+
+type Marker = { longitude: number; latitude: number; color?: number; radius?: number }
+type Props = { textureUrl?: string; selectedTime: string; markers?: Marker[]; autoRotate?: boolean }
+
+export function RealisticEarthGlobe({ selectedTime, markers = [] }: Props) {
+  return (
+    <>
+      <CleanEarthViewer selectedTime={selectedTime} markers={markers} />
+      <SatelliteSourceRegistry />
+    </>
+  )
+}
