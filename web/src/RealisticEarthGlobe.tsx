c054d78c8073322f199d7f4f15ac5f5ed793f9b6
Terraforming Planet <kontakt@teslaeco.pl>
2026-07-30 18:52:02 +0100

    fix(earth): restore always-visible 3D globe renderer

--- web/src/RealisticEarthGlobe.tsx
+++ web/src/RealisticEarthGlobe.tsx
@@ -1,13 +1,13 @@
-import { RealisticEarthGlobe as CleanEarthViewer } from './CleanRealisticEarthGlobe'
+import { StableEarthGlobe } from './StableEarthGlobe'
 import { SatelliteSourceRegistry } from './satellite-source-registry'
 
 type Marker = { longitude: number; latitude: number; color?: number; radius?: number }
 type Props = { textureUrl?: string; selectedTime: string; markers?: Marker[]; autoRotate?: boolean }
 
-export function RealisticEarthGlobe({ selectedTime, markers = [] }: Props) {
+export function RealisticEarthGlobe({ selectedTime, markers = [], autoRotate = true }: Props) {
   return (
     <>
-      <CleanEarthViewer selectedTime={selectedTime} markers={markers} />
+      <StableEarthGlobe selectedTime={selectedTime} markers={markers} autoRotate={autoRotate} />
       <SatelliteSourceRegistry />
     </>
   )
