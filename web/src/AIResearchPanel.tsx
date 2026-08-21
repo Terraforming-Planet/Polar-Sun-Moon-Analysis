debeb869f93de56551c69efc391a769dd735da1b
Terraforming Planet <kontakt@teslaeco.pl>
2026-08-21 00:21:53 +0000

    Load simple mode resource styles

--- web/src/AIResearchPanel.tsx
+++ web/src/AIResearchPanel.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react'
 
 import './ai-research.css'
 import './research-findings.css'
+import './simple-mode-resources.css'
 import { EvidenceExplainer } from './EvidenceExplainer'
 import { ResearchArchivePanel } from './ResearchArchivePanel'
 import { ResearchAreaBuilder } from './ResearchAreaBuilder'
